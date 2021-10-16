a501fb3b9ec8b413635aadabfb82e5dc941b0d96
StardustDL <stardustdl@163.com>
2021-10-16 20:08:51 +0800

    fix bugs in diff rules

--- src/main/aexpy/diffs/differ.py
+++ src/main/aexpy/diffs/differ.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
 from uuid import uuid1
-from aexpy.analyses.models import ApiCollection, ApiEntry, ApiManifest, ClassEntry, FieldEntry, FunctionEntry, ModuleEntry
+from aexpy.analyses.models import ApiCollection, ApiEntry, ApiManifest, ClassEntry, FieldEntry, FunctionEntry, ModuleEntry, Parameter, ParameterKind
 from .models import DiffCollection, DiffEntry, DiffRule
 
 
@@ -49,15 +49,37 @@ if __name__ == "__main__":
     old.addEntry(ModuleEntry(id="mod"))
     old.addEntry(ModuleEntry(id="mod0", members={"m1": "", "m2": ""}))
     old.addEntry(ClassEntry(id="cls"))
+    old.addEntry(ClassEntry(id="cls0", bases=["b1"]))
     old.addEntry(FunctionEntry(id="func"))
+    old.addEntry(FunctionEntry(id="func0", returnType="str", parameters=[
+        Parameter(ParameterKind.PositionalOrKeyword, name="a"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="c", optional=True),
+        Parameter(ParameterKind.PositionalOrKeyword, name="e"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="f", optional=True),
+        Parameter(ParameterKind.PositionalOrKeyword, name="g", optional=True, default="1"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="h", type="int"),
+        Parameter(ParameterKind.VarPositional, name="i"),
+        Parameter(ParameterKind.VarKeyword, name="j"),
+    ]))
     old.addEntry(FieldEntry(id="field"))
+    old.addEntry(FieldEntry(id="field0", type="str"))
 
     new = ApiCollection(ApiManifest("test", "2"))
     new.addEntry(ModuleEntry(id="mod2"))
     new.addEntry(ModuleEntry(id="mod0", members={"m2": "changed", "m3": ""}))
     new.addEntry(ClassEntry(id="cls2"))
+    new.addEntry(ClassEntry(id="cls0", bases=["b2"]))
     new.addEntry(FunctionEntry(id="func2"))
+    new.addEntry(FunctionEntry(id="func0", returnType="int", parameters=[
+        Parameter(ParameterKind.PositionalOrKeyword, name="b"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="d", optional=True),
+        Parameter(ParameterKind.PositionalOrKeyword, name="e", optional=True),
+        Parameter(ParameterKind.PositionalOrKeyword, name="f"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="g", optional=True, default="2"),
+        Parameter(ParameterKind.PositionalOrKeyword, name="h", type="str"),
+    ]))
     new.addEntry(FieldEntry(id="field2"))
+    new.addEntry(FieldEntry(id="field0", type="int"))
 
     result = Differ().with_default_rules().process(old, new)
 
--- src/main/aexpy/diffs/rules.py
+++ src/main/aexpy/diffs/rules.py
@@ -1,4 +1,5 @@
-from typing import Callable, Optional
+import itertools
+from typing import Callable, Optional, OrderedDict
 from aexpy.analyses.models import ApiEntry, CollectionEntry, ModuleEntry, ClassEntry, FunctionEntry, FieldEntry, Parameter, ParameterKind
 from .models import DiffRule, RuleCheckResult
 from itertools import zip_longest
@@ -42,7 +43,7 @@ def changeMember(a: Optional[CollectionEntry], b: Optional[CollectionEntry]):
 
 
 def changeBases(a: Optional[ClassEntry], b: Optional[ClassEntry]):
-    changed = a ^ b
+    changed = set(a.bases) ^ set(b.bases)
     if len(changed) > 0:
         return RuleCheckResult(True, f"{changed}")
     return RuleCheckResult.unsatisfied()
@@ -61,34 +62,47 @@ def changeReturnType(a: Optional[FunctionEntry], b: Optional[FunctionEntry]):
 
 
 def matchParameters(a: Optional[FunctionEntry], b: Optional[FunctionEntry]):
-    posA = filter(lambda x: x.kind == ParameterKind.Positional or x.kind ==
-                  ParameterKind.PositionalOrKeyword, a.parameters)
-    posB = filter(lambda x: x.kind == ParameterKind.Positional or x.kind ==
-                  ParameterKind.PositionalOrKeyword, b.parameters)
-    for x, y in zip_longest(posA, posB):
-        yield x, y
+    def inner():
+        posA = filter(lambda x: x.kind == ParameterKind.Positional or x.kind ==
+                      ParameterKind.PositionalOrKeyword, a.parameters)
+        posB = filter(lambda x: x.kind == ParameterKind.Positional or x.kind ==
+                      ParameterKind.PositionalOrKeyword, b.parameters)
+        for x, y in zip_longest(posA, posB):
+            yield x, y
 
-    kwA = {p.name: p for p in filter(lambda x: x.kind == ParameterKind.Keyword or x.kind ==
-                                     ParameterKind.PositionalOrKeyword, a.parameters)}
-    kwB = {p.name: p for p in filter(lambda x: x.kind == ParameterKind.Keyword or x.kind ==
-                                     ParameterKind.PositionalOrKeyword, b.parameters)}
+        kwA = {p.name: p for p in filter(lambda x: x.kind == ParameterKind.Keyword or x.kind ==
+                                         ParameterKind.PositionalOrKeyword, a.parameters)}
+        kwB = {p.name: p for p in filter(lambda x: x.kind == ParameterKind.Keyword or x.kind ==
+                                         ParameterKind.PositionalOrKeyword, b.parameters)}
 
-    for k, v in kwA:
-        yield v, kwB.get(k)
+        for k, v in kwA.items():
+            yield v, kwB.get(k)
 
-    for k, v in kwB:
-        if k in kwA:
+        for k, v in kwB.items():
+            if k in kwA:
+                continue
+            yield None, v
+
+        for x, y in zip_longest(
+                filter(lambda x: x.kind ==
+                       ParameterKind.VarPositional, a.parameters),
+                filter(lambda x: x.kind == ParameterKind.VarPositional, b.parameters)):
+            yield x, y
+
+        for x, y in zip_longest(
+                filter(lambda x: x.kind ==
+                       ParameterKind.VarKeyword, a.parameters),
+                filter(lambda x: x.kind == ParameterKind.VarKeyword, b.parameters)):
+            yield x, y
+
+    done = set()
+
+    for x, y in inner():
+        nx = x.name if x else None
+        ny = y.name if y else None
+        if (nx, ny) in done:
             continue
-        yield None, v
-
-    for x, y in zip_longest(
-            filter(lambda x: x.kind == ParameterKind.VarPositional, a.parameters),
-            filter(lambda x: x.kind == ParameterKind.VarPositional, b.parameters)):
-        yield x, y
-
-    for x, y in zip_longest(
-            filter(lambda x: x.kind == ParameterKind.VarKeyword, a.parameters),
-            filter(lambda x: x.kind == ParameterKind.VarKeyword, b.parameters)):
+        done.add((nx, ny))
         yield x, y
 
 
@@ -99,7 +113,7 @@ def changeParameter(checker: Callable[[Optional[Parameter], Optional[Parameter]]
             result = checker(x, y)
             if result:
                 results.append(
-                    f"{a.name if a else 'None'} & {b.name if b else 'None'}{': ' + result.message if result.message else ''}")
+                    f"{x.name if x else 'None'} & {y.name if y else 'None'}{': ' + result.message if result.message else ''}")
         return RuleCheckResult(len(results) > 0, str(results))
 
     return wrapper
@@ -160,7 +174,14 @@ def removePD(a: Optional[Parameter], b: Optional[Parameter]):
 @changeParameter
 def changePD(a: Optional[Parameter], b: Optional[Parameter]):
     if a is not None and b is not None and a.optional and b.optional and a.default != b.default:
-        return RuleCheckResult.satisfied()
+        return RuleCheckResult(True, f"{a.default} -> {b.default}")
+    return RuleCheckResult.unsatisfied()
+
+
+@changeParameter
+def changePT(a: Optional[Parameter], b: Optional[Parameter]):
+    if a is not None and b is not None and a.type != b.type:
+        return RuleCheckResult(True, f"{a.type} -> {b.type}")
     return RuleCheckResult.unsatisfied()
 
 
@@ -207,7 +228,7 @@ pararules = [
     DiffRule("AddParameterDefault", addPD).fortype(FunctionEntry),
     DiffRule("RemoveParameterDefault", removePD).fortype(FunctionEntry),
     DiffRule("ChangeParameterDefault", changePD).fortype(FunctionEntry),
-    DiffRule("ChangeParamterType", changeFieldType).fortype(FunctionEntry),
+    DiffRule("ChangeParamterType", changePT).fortype(FunctionEntry),
     DiffRule("RemoveVarPositional", removeVP).fortype(FunctionEntry),
     DiffRule("RemoveVarKeyword", removeVKP).fortype(FunctionEntry),
 ]
