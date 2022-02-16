1fb78ed5f89ada1fce61f5d0268449f870be6f80
StardustDL <stardustdl@163.com>
2022-02-16 11:41:33 +0800

    add pipeline class

--- src/aexpy/pipelines.py
+++ src/aexpy/pipelines.py
@@ -5,21 +5,25 @@ from .differing import Differ, getDefault as getDefaultDiffer
 from .evaluating import Evaluator, getDefault as getDefaultEvaluator
 
 
-def preprocess(release: "Release", preprocessor: "Preprocessor | None" = None) -> "Distribution":
-    preprocessor = preprocessor or getDefaultPreprocessor()
-    return preprocessor.preprocess(release)
+class Pipeline:
+    def __init__(self, preprocessor: "Preprocessor | None" = None, extractor: "Extractor | None" = None, differ: "Differ | None" = None, evaluator: "Evaluator | None" = None) -> None:
+        self.preprocessor = preprocessor or getDefaultPreprocessor()
+        self.extractor = extractor or getDefaultExtractor()
+        self.differ = differ or getDefaultDiffer()
+        self.evaluator = evaluator or getDefaultEvaluator()
 
+    def preprocess(self, release: "Release", preprocessor: "Preprocessor | None" = None) -> "Distribution":
+        preprocessor = preprocessor or self.preprocessor
+        return preprocessor.preprocess(release)
 
-def extract(release: "Release", extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiDescription":
-    extractor = extractor or getDefaultExtractor()
-    return extractor.extract(preprocessor(release, preprocessor))
+    def extract(self, release: "Release", extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiDescription":
+        extractor = extractor or self.extractor
+        return extractor.extract(self.preprocess(release, preprocessor))
 
+    def diff(self, old: "Release", new: "Release", differ: "Differ | None" = None, extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiDifference":
+        differ = differ or self.differ
+        return differ.diff(self.extract(old, extractor, preprocessor), self.extract(new, extractor, preprocessor))
 
-def diff(old: "Release", new: "Release", differ: "Differ | None" = None, extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiDifference":
-    differ = differ or getDefaultDiffer()
-    return differ.diff(extract(old, extractor, preprocessor), extract(new, extractor, preprocessor))
-
-
-def eval(old: "Release", new: "Release", evaluator: "Evaluator | None" = None, differ: "Differ | None" = None, extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiBreaking":
-    evaluator = evaluator or getDefaultEvaluator()
-    return evaluator.eval(diff(old, new, differ, extractor, preprocessor))
+    def eval(self, old: "Release", new: "Release", evaluator: "Evaluator | None" = None, differ: "Differ | None" = None, extractor: "Extractor | None" = None, preprocessor: "Preprocessor | None" = None) -> "ApiBreaking":
+        evaluator = evaluator or self.evaluator
+        return evaluator.eval(self.diff(old, new, differ, extractor, preprocessor))
