fbcd57dc9f2344b2600df26c6f22fd13435d969d
StardustDL <stardustdl@163.com>
2022-03-09 12:42:39 +0800

    adjust rerun script

--- rerun.sh
+++ rerun.sh
@@ -1,6 +1,6 @@
 name=$(date -I)
 rm -rf ./logs/*
+docker stop aexpy
 cb build:docker
-python -u -m coxbuild data:cleanall
-nohup python -u -m coxbuild -c docker=aexpy -c provider=all -c project=all data:work > ./logs/$name.log 2>&1 &
-nohup python -u -m coxbuild -c docker=aexpy -c provider=default -c project=all data:work > ./logs/default.log 2>&1 &
+python -u -m coxbuild data:clean
+nohup python -u -m coxbuild -c docker=aexpy -c provider=default -c project=all data:work > ./logs/$name.log 2>&1 &
