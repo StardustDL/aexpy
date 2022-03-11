bf046dbe700c6e18ffd27ff75f578ab8eba49116
StardustDL <stardustdl@163.com>
2022-03-11 15:05:19 +0800

    remove old code

--- summary/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent.joinpath("src")))
-
-defaultProjects = ["urllib3", "python-dateutil", "requests", "pyyaml", "jmespath", "click",
-                   "coxbuild", "schemdule", "flask", "tornado", "scrapy", "numpy", "pandas", "django"]
-
-cacheRoot = Path("summary-logs")
