38057bf59256345d32f70987bb92434a4c3540c2
StardustDL <stardustdl@163.com>
2022-10-11 18:23:38 +0800

    add auto ci dev script

---
+++ exps/ci.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+import subprocess
+import os
+import sys
+from pathlib import Path
+import logging
+import time
+import traceback
+
+LOGGING_FORMAT = "%(levelname)s %(asctime)s %(name)s [%(pathname)s:%(lineno)d:%(funcName)s]\n%(message)s\n"
+LOGGING_DATEFMT = "%Y-%m-%d,%H:%M:%S"
+
+logPath = Path(".") / "ci.log"
+logFile = None
+
+scheduleHour = 1
+CYCLE_SECOND = 1
+
+
+def log(s: str):
+    ts = f"{datetime.now()}: {s}"
+    print(ts)
+    print(ts, file=logFile)
+
+
+def onCommit():
+    log("New commit detected.")
+
+
+def onSchedule():
+    log("Schedule task.")
+
+
+def getCommitId():
+    return subprocess.check_output("git rev-parse HEAD", text=True).strip()
+
+
+def eventloop():
+    lastCommit = getCommitId()
+    scheduleDay = -1
+    while True:
+        time.sleep(CYCLE_SECOND)
+        try:
+            currentCommit = getCommitId()
+            if currentCommit != lastCommit:
+                log(f"New commit {currentCommit} from {lastCommit}.")
+                onCommit()
+                lastCommit = currentCommit
+            now = datetime.now()
+            if now.hour == scheduleHour and scheduleDay != now.day:
+                onSchedule()
+                scheduleDay = now.day
+        except Exception as ex:
+            log(f"Exception: {'. '.join(traceback.format_exception(ex))}")
+
+
+def main():
+    global logFile
+    with logPath.open("w") as log:
+        logFile = log
+        eventloop()
+
+
+if __name__ == "__main__":
+    main()
