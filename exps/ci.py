8cd5a3ebbd8717bc33eb6201d8b2d719c702d4bd
StardustDL <stardustdl@163.com>
2022-10-11 18:51:30 +0800

    remove clean in dev ci

--- exps/ci.py
+++ exps/ci.py
@@ -31,7 +31,7 @@ def onCommit():
 
 def onSchedule():
     log("Schedule task.")
-    subprocess.check_call("cb data:clean".split())
+    # subprocess.check_call("cb data:clean".split())
     subprocess.check_call("cb -c docker=aexpy/aexpy -c provider=default -c project=click data:work".split())
 
 
--- exps/ci.sh
+++ exps/ci.sh
@@ -1,2 +1,3 @@
 sudo ls
 nohup sudo python exps/ci.py > ./logs/ciraw.log 2>&1 &
+ps -A -f | grep python
