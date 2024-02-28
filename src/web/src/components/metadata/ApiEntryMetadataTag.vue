1aa93ad688b1219c21108326b76a96728c9d1a06
StardustDL <stardustdl@163.com>
2024-02-28 19:25:35 +0800

    web: fix tag

--- src/web/src/components/metadata/ApiEntryMetadataTag.vue
+++ src/web/src/components/metadata/ApiEntryMetadataTag.vue
@@ -13,15 +13,15 @@ defineProps<{
     <n-tag v-if="(entry instanceof ItemEntry)" :type="entry.scope != ItemScope.Static ? 'warning' : 'info'">{{
         ItemScope[entry.scope]
     }}</n-tag>
-    <n-tag v-if="(entry instanceof ClassEntry && entry.flags | ClassFlag.Abstract)" type="error">Abstract</n-tag>
-    <n-tag v-if="(entry instanceof ClassEntry && entry.flags | ClassFlag.Final)" type="warning">Final</n-tag>
-    <n-tag v-if="(entry instanceof ClassEntry && entry.flags | ClassFlag.Generic)" type="info">Generic</n-tag>
-    <n-tag v-if="(entry instanceof ClassEntry && entry.flags | ClassFlag.Dataclass)" type="info">Dataclass</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.Abstract)" type="error">Abstract</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.Final)" type="warning">Final</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.Generic)" type="info">Generic</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.Override)" type="success">Override</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.Async)" type="error">Async</n-tag>
-    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags | FunctionFlag.TransmitKwargs)">Transmit Kwargs</n-tag>
+    <n-tag v-if="(entry instanceof ClassEntry && entry.flags & ClassFlag.Abstract)" type="error">Abstract</n-tag>
+    <n-tag v-if="(entry instanceof ClassEntry && entry.flags & ClassFlag.Final)" type="warning">Final</n-tag>
+    <n-tag v-if="(entry instanceof ClassEntry && entry.flags & ClassFlag.Generic)" type="info">Generic</n-tag>
+    <n-tag v-if="(entry instanceof ClassEntry && entry.flags & ClassFlag.Dataclass)" type="info">Dataclass</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.Abstract)" type="error">Abstract</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.Final)" type="warning">Final</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.Generic)" type="info">Generic</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.Override)" type="success">Override</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.Async)" type="error">Async</n-tag>
+    <n-tag v-if="(entry instanceof FunctionEntry && entry.flags & FunctionFlag.TransmitKwargs)">Transmit Kwargs</n-tag>
     <n-tag v-if="(entry instanceof AttributeEntry && entry.property)" type="success">Property</n-tag>
 </template>
